dd38e8fe7003b19eff08b7a7e8a195d28f62309b
tylerpudiwitr <tyler.pudiwitr@outlook.com>
2022-10-09 13:43:58 -0500

    Capstone

--- Components/views/index.js
+++ Components/views/index.js
@@ -1,4 +1,2 @@
 export { default as Bio } from "./Bio";
 export { default as Home } from "./Home";
-export { default as Order } from "./Order";
-export { default as Pizza } from "./Pizza";
--- store/index.js
+++ store/index.js
@@ -1,5 +1,1 @@
 export { default as Home } from "./Home";
-export { default as Bio } from "./Bio";
-export { default as Order } from "./Order";
-export { default as Tunes } from "./inTune";
-export { default as Links } from "./Links";
