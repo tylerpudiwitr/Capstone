45e39f656fdc3588b78d218544b0e73fedf74cd0
tylerpudiwitr <tyler.pudiwitr@outlook.com>
2022-10-13 21:05:32 -0500

    audioAPI

--- player.js
+++ player.js
@@ -1,14 +1,43 @@
 {
   class AudioPlayer extends HTMLElement {
+    playing = false;
+
     constructor() {
       super();
 
       this.attachShadow({ mode: `open` });
       this.render();
+      this.initializeAudio();
+      this.attachEvents();
     }
 
     initializeAudio() {
       this.AudioContext = new AudioContext();
+
+      this.track = this.AudioContext.createMediaElementSource(this.audio);
+
+      this.track.connect(this.AudioContext.destination);
+    }
+
+    attachEvents() {
+      this.playPauseBtn.addEventListener("click", this.togglePlay.bind(this))
+    }
+
+    async togglePlay() {
+      if (this.AudioContext.state === "suspended") {
+        await this.AudioContext.resume();
+      this.playPauseBtn.textContent = "play";
+      }
+
+      if (this.playing) {
+          await this.audio.pause();
+          this.playing = false;
+      } else {
+          await this.audio.play();
+          this.playing = true;
+          this.playPauseBtn.textContent = "pause";
+      }
+
     }
 
     render() {
@@ -18,6 +47,7 @@
     `;
 
       this.audio = this.shadowRoot.querySelector("audio");
+      this.playPauseBtn = this.shadowRoot.querySelector(".play-btn");
     }
   }
 
