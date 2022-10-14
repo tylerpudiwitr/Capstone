4e8e5fbea35f53feabf40876e3ede02e17df8a39
tylerpudiwitr <tyler.pudiwitr@outlook.com>
2022-10-13 21:30:12 -0500

    audioAPI

--- player.js
+++ player.js
@@ -1,6 +1,8 @@
 {
   class AudioPlayer extends HTMLElement {
     playing = false;
+    currentTime = 0;
+    duration = 0;
 
     constructor() {
       super();
@@ -21,6 +23,19 @@
 
     attachEvents() {
       this.playPauseBtn.addEventListener("click", this.togglePlay.bind(this))
+
+      this.audio.addEventListener(`loadedmetadata`, () => {
+        this.duration = this.audio.duration;
+        this.progressBar.max = this.duration;
+
+        const secs = parseInt(`${this.duration % 60}`, 10);
+        const mins = parseInt(`${(this.duration/60)% 60}`, 10);
+
+        this.durationEl.textContent = `${mins}:${secs}`;
+
+        console.log("duration", this.audio.duration);
+        console.log("currentTime", this.audio.currentTime);
+      })
     }
 
     async togglePlay() {
@@ -42,12 +57,22 @@
 
     render() {
       this.shadowRoot.innerHTML = `
-    <audio src="MVP.mp3" controls></audio>
-    <button class="play-btn" type="button">play</button>
+        <audio src="MVP.mp3" controls></audio>
+        <button class="play-btn" type="button">play</button>
+        <div class="progress-indicator">
+            <span class="current-time">0:00</span>
+            <input type="range" max="100" value="0" class="progress-bar">
+            <span class="duration">0:00</span>
+        </div>
     `;
 
       this.audio = this.shadowRoot.querySelector("audio");
       this.playPauseBtn = this.shadowRoot.querySelector(".play-btn");
+      this.progressIndicator = this.shadowRoot.querySelector(".progress-indicator");
+      this.currentTimeEl = this.progressIndicator.children[0];
+      this.progressBar = this.progressIndicator.children[1];
+      this.durationEl = this.progressIndicator.children[2];
+
     }
   }
 
