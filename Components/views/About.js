a13b0a0feab3addd9017ed21ecf973f3cd645a67
tylerpudiwitr <tyler.pudiwitr@outlook.com>
2022-10-19 12:13:55 -0500

    cleaningup

--- Components/views/About.js
+++ Components/views/About.js
@@ -4,6 +4,13 @@ import html from "html-literal";
 export default state => html`
   <section id="about">
     <h2>About Us/Our Mission</h2>
+    <p>
+      Our goal is to help out newer upcoming artists, or people that are curious
+      about making music, mix and master their own songs. We know that
+      professional studio time can be really expensive, so let's cut the costs
+      by learning how to do it ourselves. You can record anywhere! In your
+      bedroom, living room, or even your car!
+    </p>
     <div id="error"></div>
     <form action="/" method="GET">
       <div>
@@ -11,18 +18,11 @@ export default state => html`
         <input id="name" name="name" type="text" required />
       </div>
       <div>
-        <label for="password">Password</label>
-        <input id="password" name="password" type="password" />
+        <label for="email">E-mail</label>
+        <input id="email" name="email" type="email" />
       </div>
       <button type="submit">Submit</button>
     </form>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu sagittis
-      nulla. Etiam semper mauris a quam tempus, non feugiat massa posuere. Morbi
-      vitae nisi aliquam, semper diam id, molestie leo. In hac habitasse platea
-      dictumst. Nulla et enim vel elit dictum viverra. Nunc pharetra lacinia
-      arcu sit amet dignissim. Pellentesque ut mi nulla.
-    </p>
     <img
       src="https://cdn.mos.cms.futurecdn.net/doXGMAjXPZaCpe9tjkFKsd.jpg"
       alt="studio"
