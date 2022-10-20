ec9f39c683a528e7906dc6ead2a74b4fe09267a3
tylerpudiwitr <tyler.pudiwitr@outlook.com>
2022-10-19 21:39:07 -0500

    capstone

--- Components/views/Home.js
+++ Components/views/Home.js
@@ -2,13 +2,18 @@ import html from "html-literal";
 
 export default state => html`
 <body>
-  <div>
-<ul class="nav_links">
-  <li><a href="Home">Home</a></li>
-  <li><a href="About">About Us</a></li>
-  <li><a href="Recording">Recording Your Vocals</a></li>
-</ul>
-</div>
+  <nav class="navbar">
+    <label class="navbar-toggler" for="toggle">
+      <span class="bar"></span>
+      <span class="bar"></span>
+      <span class="bar"></span>
+    </label>
+    <ul class="nav_list">
+      <li class="nav-item"><a href="Home">Home</a></li>
+      <li class="nav-item"><a href="About">About Us</a></li>
+      <li class="nav-item"><a href="Recording">Recording</a></li>
+    </ul>
+  </nav>
     <section id="jumbotron"></section>
     <p>
       The weather in ${state.weather.city} is ${state.weather.description}.
