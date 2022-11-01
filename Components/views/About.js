90bf022f1f5ba505d4d6faa4fc040609b7361309
tylerpudiwitr <tyler.pudiwitr@outlook.com>
2022-10-31 23:10:52 -0500

    capstone

--- Components/views/About.js
+++ Components/views/About.js
@@ -5,6 +5,14 @@ import html from "html-literal";
 export default state => html`
   <div id="pageContents"></div>
   <div id="about">
+  <h5>In the future we plan on creating a system that will
+      allow Artists and Producers to connect with each other directly on our app.
+      This will help Artists learn more about mixing and mastering as they will
+      be creating a song together in real time!
+
+        If you have any questions or comments we hope you reach out to us so we
+      can assist you as best as we can! And remember, consistency is the key.
+    </h5>
     <h2>OUR MISSION</h2>
     <p>
       Our goal is to help out newer upcoming artists, or people that are curious
@@ -17,18 +25,20 @@ export default state => html`
       Reach out to us by entering your full name and email! Response should be
       expected within 24-48 hours!
     </h4>
-    <div id="error"></div>
+    <div id="error">
     <form action="https://formspree.io/f/xzbwaayo" method="POST">
       <div>
         <h3>ENTER YOUR NAME</h3>
         <h3>AND EMAIL SO WE CAN REACH OUT!</h3>
-        <label class="names" for="name">Name</label>
+        <label class="names" for="name">NAME</label>
         <input id="name" name="name" type="text" required />
-        <div></div>
-        <label class="mail" for="email">Email</label>
-        <input id="email" name="email" type="email" required />
       </div>
-
+      <div>
+      <label class="mail" for="email">EMAIL</label>
+      <input id="email" name="email" type="email" required />
+      </div>
+      <label class="question" for="question">Question/Comment</label>
+    <input id="question" name="question" type="question" required />
       <button class="sub" type="submit">Submit</button>
     </form>
     <img
