9411961609dd413fd0b7cbfd864ef6b566d90af1
tylerpudiwitr <tyler.pudiwitr@outlook.com>
2022-11-01 17:04:42 -0500

    capstone

--- Components/views/About.js
+++ Components/views/About.js
@@ -5,13 +5,13 @@ import html from "html-literal";
 export default state => html`
   <div id="pageContents"></div>
   <div id="about">
-  <h5>In the future we plan on creating a system that will
-      allow Artists and Producers to connect with each other directly on our app.
-      This will help Artists learn more about mixing and mastering as they will
-      be creating a song together in real time!
-
-        If you have any questions or comments we hope you reach out to us so we
-      can assist you as best as we can! And remember, consistency is the key.
+    <h5>
+      In the future we plan on creating a system that will allow Artists and
+      Producers to connect with each other directly on our app. This will help
+      Artists learn more about mixing and mastering as they will be creating a
+      song together in real time! If you have any questions or comments we hope
+      you reach out to us so we can assist you as best as we can! And remember,
+      consistency is the key.
     </h5>
     <h2 class="mission">OUR MISSION</h2>
     <p class="goal">
@@ -22,25 +22,26 @@ export default state => html`
       bedroom, living room, or even your car!
     </p>
     <div id="error">
-    <form action="https://formspree.io/f/xzbwaayo" method="POST">
-      <div>
-        <h3>ENTER YOUR NAME</h3>
-        <h3>AND EMAIL SO WE CAN REACH OUT!</h3>
-        <label class="names" for="name">NAME</label>
-        <input id="name" name="name" type="text" required />
-      </div>
-      <div>
-      <label class="mail" for="email">EMAIL</label>
-      <input id="email" name="email" type="email" required />
-      </div>
-      <label class="question" for="question">Question/Comment</label>
-    <input id="question" name="question" type="question" required />
-      <button class="sub" type="submit">Submit</button>
-    </form>
-    <img
-      class="homepage"
-      src="https://cdn.mos.cms.futurecdn.net/doXGMAjXPZaCpe9tjkFKsd.jpg"
-      alt="studio"
-    />
-  </section>
+      <form action="https://formspree.io/f/xzbwaayo" method="POST">
+        <div>
+          <h3 class="fill">ENTER YOUR NAME</h3>
+          <h3 class="fill">AND EMAIL SO WE CAN REACH OUT!</h3>
+          <label class="names" for="name">NAME</label>
+          <input id="name" name="name" type="text" required />
+        </div>
+        <div>
+          <label class="mail" for="email">EMAIL</label>
+          <input id="email" name="email" type="email" required />
+        </div>
+        <label class="question" for="question">Question/Comment</label>
+        <input id="question" name="question" type="question" required />
+        <button class="sub" type="submit">Submit</button>
+      </form>
+      <img
+        class="homepage"
+        src="https://cdn.mos.cms.futurecdn.net/doXGMAjXPZaCpe9tjkFKsd.jpg"
+        alt="studio"
+      />
+    </div>
+  </div>
 `;
